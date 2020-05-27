01250d61f6063f053ae1b66d73bbd685f919759a
Vedant Pandey <vedantpandey46@gmail.com>
2020-05-27 13:38:08 +0530

    Add cat-file and hash-object commands

--- libumpire.py
+++ libumpire.py
@@ -11,16 +11,6 @@ argparser = argparse.ArgumentParser(description="Umpire version control manager"
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
-# CLI argument parser for init command
-argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
-argsp.add_argument(
-	"path", 
-	metavar="directory",
-	nargs="?", 
-	default=".", 
-	help="Where to create he repository"
-)
-
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
 
@@ -39,18 +29,18 @@ def main(argv=sys.argv[1:]):
 	elif args.command == "show-ref"    : cmd_show_ref(args)
 	elif args.command == "tag"         : cmd_tag(args)
 
-class UmpRepo(object):
+class GitRepository(object):
 	"""An umpire repository"""
 
 	worktree = None
-	umpDir = None
+	gitDir = None
 	conf = None
 
 	def __init__(self, path, force=False):
 		self.worktree = path
-		self.umpDir = os.path.join(path, ".ump")
+		self.gitDir = os.path.join(path, ".git")
 
-		if not (force or os.path.isdir(self.umpDir)):
+		if not (force or os.path.isdir(self.gitDir)):
 			raise Exception("Not a working umpire repository %s" % path)
 
 		# Read config file
@@ -70,12 +60,12 @@ class UmpRepo(object):
 
 
 def repo_path(repo, *path):
-	return os.path.join(repo.umpDir, *path)
+	return os.path.join(repo.gitDir, *path)
 
 def repo_file(repo, *path, mkdir=False):
 	"""Same as repo_path, but create dirname(*path) if absent. For example
 	repo_file(r, 'refs', 'remotes', 'origin', 'HEAD') will create
-	.ump/refs/remotes/origin """
+	.git/refs/remotes/origin """
 
 	if repo_dir(repo, *path[:-1], mkdir=mkdir):
 		return repo_path(repo, *path)
@@ -89,7 +79,7 @@ def repo_dir(repo, *path, mkdir=False):
 		if os.path.isdir(path):
 			return path
 		else:
-			raise Exception("Not a working umpire repository %s" % path)
+			raise Exception("Not a working repository %s" % path)
 
 	if mkdir:
 		os.makedirs(path)
@@ -100,7 +90,7 @@ def repo_dir(repo, *path, mkdir=False):
 def repo_create(path):
 	"""Initialize a new repository at path."""
 
-	repo = UmpRepo(path, True)
+	repo = GitRepository(path, True)
 
 	# Check the paths don't already exist
 
@@ -109,7 +99,7 @@ def repo_create(path):
 			raise Exception("%s is not a directory" % path)
 
 		if os.listdir(repo.worktree):
-			raise Exception("%s is not empty" % path)
+			raise Exception("%s is not empty" % os.path.realpath(path))
 
 	else:
 		os.makedirs(repo.worktree)
@@ -143,6 +133,16 @@ def repo_default_config():
 
 	return ret
 
+# CLI argument parser for init command
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument(
+	"path", 
+	metavar="directory",
+	nargs="?", 
+	default=".", 
+	help="Where to create the repository"
+)
+
 
 # Init command handler
 def cmd_init(args):
@@ -151,17 +151,164 @@ def cmd_init(args):
 def repo_find(path=".", required=True):
 	path = os.path.realpath(path)
 
-	if os.path.isdir(os.path.join(path, ".ump")):
-		return UmpRepo(path)
+	if os.path.isdir(os.path.join(path, ".git")):
+		return GitRepository(path)
 
-	# Recurse parent if current directory is not the main git directory
+	# Recurse parent if current directory is not the main umpire directory
 	parent = os.path.realpath(os.path.join(path, ".."))
 
 	# Base case if current directory is system root
 	if parent == path:
 		if required:
-			raise Exception("No upmire repository found")
+			raise Exception("No repository found")
 		else:
 			return None
 
 	return repo_find(parent, required)
+
+class GitObject(object):
+	repo = None
+
+	def __init__(self, repo, data=None):
+		self.repo=repo
+
+		if data != None:
+			self.deserialize(data)
+
+	def serialize(self):
+		"""Abstract function to be implemented by sub classes
+		The data class member contains information of the object as byte string"""
+		raise Exception("Unimplemented method")
+
+	def deserialize(self, data):
+		raise Exception("Unimplemented method")
+
+def object_read(repo, sha):
+	"""Read object id from repo and return GitObject"""
+
+	path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+	with open (path, "rb") as f:
+		raw = zlib.decompress(f.read())
+
+		x = raw.find(b' ')
+		fmt = raw[0:x]
+
+		y = raw.find(b'\x00', x)
+		size = int(raw[x:y].decode("ascii"))
+		if size != len(raw) - y - 1:
+			raise Exception("Malformed object {0}: bad length".format(sha))
+
+		if fmt == b'commit' : c=GitCommit
+		elif fmt == b'tree' : c=GitTree
+		elif fmt == b'tag'  : c=GitTag
+		elif fmt == b'blob' : c=GitBlob
+		else:
+			raise Exception("Unknown type %s for object %s".format(fmt.decode("ascii"), sha))
+
+		return c(repo, raw[y + 1:])
+
+# Placeholder function
+def object_find(repo, name, fmt=None, follow=True):
+	return name
+
+def object_write(obj, true_write=True):
+	# Serialize data
+	data = obj.serialize()
+
+	# Set header
+	result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+
+	# Compute hash
+	sha = hashlib.sha1(result).hexdigest()
+
+	if true_write:
+		# Create path
+		path = repo_file(obj.repo, "objects", sha[0:2], sha[2:], mkdir=true_write)
+
+		with open(path, "wb") as f:
+			f.write(zlib.compress(result))
+	
+	return sha
+
+class GitBlob(GitObject):
+	fmt = b'blob'
+
+	def serialize(self):
+		return self.blobdata
+
+	def deserialize(self, data):
+		self.blobdata = data
+
+# CLI argument parser for cat-file command
+argsp = argsubparsers.add_parser("cat-file", help="Display content of repository objects")
+
+argsp.add_argument(
+	"type",
+	metavar="type",
+	choices=["blob", "commit", "tag", "tree"],
+	help="Specify the type"
+)
+
+argsp.add_argument(
+	"object",
+	metavar="object",
+	help="Object to be displayed"
+)
+
+def cmd_cat_file(args):
+	repo = repo_find()
+	cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+	obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+	sys.stdout.buffer.write(obj.serialize())
+
+# CLI argument parser for hash-object command
+argsp = argsubparsers.add_parser(
+	"hash-object",
+	help="Compute object ID and optionally creates a blob from a file"
+)
+
+argsp.add_argument(
+	"-t",
+	metavar="type",
+	dest="type",
+	choices=["blob", "commit", "tag", "tree"],
+	default="blob",
+	help="Specify type of file to be hashed"
+)
+
+argsp.add_argument(
+	"-w",
+	dest="write",
+	action="store_true",
+	help="Write to hash of object into storage"
+)
+
+argsp.add_argument(
+	"path",
+	help="Read object from <file>"
+)
+
+def cmd_hash_object(args):
+	if args.write:
+		repo = GitRepository(".")
+	else:
+		repo=None
+
+	with open(args.path, "rb") as fd:
+		sha = object_hash(fd, args.type.encode(), repo)
+		print(sha)
+
+def object_hash(fd, fmt, repo=None):
+	data = fd.read()
+
+	if   fmt == b'commit': obj = GitCommit(repo, data)
+	elif fmt == b'tree'  : obj = GitTree(repo, data)
+	elif fmt == b'tag'   : obj = GitTag(repo, data)
+	elif fmt == b'blob'  : obj = GitBlob(repo, data)
+	else: 
+		raise Exception("Unknown type %s" % fmt)
+
+	return object_write(obj, repo)
