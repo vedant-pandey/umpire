8253d16ab67acc3bcef40ce5db53ffb73d379378
Vedant Pandey <vedantpandey46@gmail.com>
2020-05-27 23:51:38 +0530

    Add tag and ref objects and their commands

--- libumpire.py
+++ libumpire.py
@@ -508,3 +508,88 @@ def tree_checkout(repo, tree, path):
 		elif obj.fmt == b'blob':
 			with open(dest, "wb") as f:
 				f.write(obj.blobdata)
+
+def ref_resolve(repo, ref):
+	with open(repo_file(repo, ref), "r") as fp:
+		data = fp.read()[:-1]
+
+	if data.startswith("ref: "):
+		return ref_resolve(repo, data[5:])
+	else:
+		return data
+
+def ref_list(repo, path=None):
+	if not path:
+		path = repo_dir(repo, "refs")
+	
+	ret = collections.OrderedDict()
+
+	for f in sorted(os.listdir(path)):
+		can = os.path.join(path, f)
+		if os.path.isdir(can):
+			ret[f] = ref_list(repo, can)
+		else:
+			ret[f] = ref_resolve(repo, can)
+	
+	return ret
+
+argsp = argsubparsers.add_parser("show-ref", help="List references")
+
+def cmd_show_ref(args):
+	repo = repo_find()
+	refs = ref_list(repo)
+	show_ref(repo, refs, prefix="refs")
+
+def show_ref(repo, refs, with_hash=True, prefix=""):
+	for k, v in refs.items():
+		if type(v) == str:
+			print("{0}{1}{2}".format(
+				v + " " if with_hash else "",
+				prefix + "/" if prefix else "",
+				k
+			))
+
+		else:
+			show_ref(repo, v, with_hash=with_hash, prefix="{0}{1}{2}".format(prefix, "/" if prefix else "", k))
+
+class GitTag(GitCommit):
+	fmt = b'tag'
+
+argsp = argsubparsers.add_parser(
+		"tag",
+		help="List and create tags"
+)
+
+argsp.add_argument(
+	"-a",
+	action="store_true",
+	dest="create_tag_object",
+	help="Whether to create a tag object"
+)
+
+argsp.add_argument(
+	"name",
+	nargs="?",
+	help="The new tag's name"
+)
+
+argsp.add_argument(
+	"object",
+	default="HEAD",
+	nargs="?",
+	help="The object the new tag will point to"
+)
+
+def cmd_tags(args):
+	repo = repo_find()
+
+	if args.name:
+		tag_create(
+			args.name,
+			args.object,
+			type="object" if args.create_tag_object else "ref"
+		)
+
+	else:
+		refs = ref_list(repo)
+		show_ref(repo, refs["tags"], with_hash=False)
